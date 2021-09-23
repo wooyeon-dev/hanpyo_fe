9209052f6d31561e161ed365df2e2833d8797724
Do-ho <hong7511059@gmail.com>
2021-09-23 22:12:24 +0900

    feat: webpack 설정 파일 작성
    - require문을 사용하기 위한 no-var-requires 옵션을 파일에서만 off
    - 다른 파일에서는 require를 사용하지 못하게 위해 해당 파일에서만 off

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/index.tsx"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[contenthash].js"
+  },
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback: true,
+    port: 3000
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "url-loader",
+          options: {
+            publicPath: "/",
+            name: "[name].[ext]?[contenthash]",
+            limit: 10000
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./public/index.html"
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin()
+  ]
+};
