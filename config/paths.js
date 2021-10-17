30d68a8236e7e1033519766a50b6a66ed63d1bd3
woojin-shin <kr20828@navercorp.com>
2021-10-17 18:22:33 +0900

    chore: 웹팩 publicPath auto로 변경

--- config/paths.js
+++ config/paths.js
@@ -7,7 +7,7 @@ const getDir = (relativePath) => path.resolve(root, relativePath);
 const paths = {
   src: getDir("src"),
   buildPath: getDir("dist"),
-  publicPath: "/",
+  publicPath: "auto",
   templatePath: getDir("public")
 };
 
