65d37aff2e35638f666cccfdea98d3bb93d1b361
Do-ho <hong7511059@gmail.com>
2021-10-17 15:50:02 +0900

    feat: 프로젝트 pages 생성

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+function MyPage(): ReactElement {
+  return <div>Mypage</div>;
+}
+
+export default MyPage;
---
+++ src/pages/ReviewPage.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+function ReviewPage(): ReactElement {
+  return <div>Reviewpage</div>;
+}
+
+export default ReviewPage;
