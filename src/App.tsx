4d7f08a852e80df50735be0e61621ca7be9c9dd7
jinhyukoo <faill456@naver.com>
2021-10-21 16:17:49 +0900

    feat: recoil 사용하여 예제 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React, { ReactElement } from "react";
-
+import { useRecoilState } from "recoil";
+import { textState } from "./model/common";
 import styles from "./index.scss";
 
 function App(): ReactElement {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: any) => {
+    setText(event.target.value);
+  };
+
   return (
     <div>
+      <input type="text" value={text} onChange={onChange} />
       <h1 className={styles.text}> Hanpyo </h1>
     </div>
   );
