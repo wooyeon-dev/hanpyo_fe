69bbc17aaac38c41fe862e6faf884185da18b125
jinhyukoo <faill456@naver.com>
2021-11-03 20:37:50 +0900

    chore: 폴더 네이밍 변경

--- src/store/common.ts
+++ src/store/common.ts
@@ -1,5 +1,5 @@
 import { atom } from "recoil";
-import { COMMON_KEY } from "./utils/key";
+import { COMMON_KEY } from "./constants/key";
 
 export const textState = atom<string>({
   key: COMMON_KEY.TEXT_STATE,
