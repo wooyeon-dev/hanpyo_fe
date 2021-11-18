d0c1d6d40816e7ddf14f009b031a476461853298
woojin-shin <kr20828@navercorp.com>
2021-11-18 13:13:58 +0900

    chore: 웹팩 환경변수 설정 추가

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,24 +1,26 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config.common");
 
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "eval",
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          "style-loader",
-          { loader: "css-loader", options: { modules: { localIdentName: "[local]__[hash:base64:5]" } } },
-          { loader: "sass-loader", options: { sourceMap: true } }
-        ]
-      }
-    ]
-  }
-});
+module.exports = (env) => {
+  return merge(common(env), {
+    mode: "development",
+    devtool: "eval",
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/,
+          use: [
+            "style-loader",
+            { loader: "css-loader", options: { modules: { localIdentName: "[local]__[hash:base64:5]" } } },
+            { loader: "sass-loader", options: { sourceMap: true } }
+          ]
+        }
+      ]
+    }
+  });
+};
--- config/webpack/webpack.config.prod.js
+++ config/webpack/webpack.config.prod.js
@@ -2,16 +2,18 @@ const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const common = require("./webpack.config.common");
 
-module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, { loader: "css-loader", options: { modules: true } }, "sass-loader"]
-      }
-    ]
-  },
-  plugins: [new MiniCssExtractPlugin({ filename: "style.css" })]
-});
+module.exports = (env) => {
+  return merge(common(env), {
+    mode: "production",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/,
+          use: [MiniCssExtractPlugin.loader, { loader: "css-loader", options: { modules: true } }, "sass-loader"]
+        }
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: "style.css" })]
+  });
+};
