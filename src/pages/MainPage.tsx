b0271374af3d769107002dd75418ef6a0272314c
Do-ho <hong7511059@gmail.com>
2021-12-12 22:15:08 +0900

    feat: MainPage 레이아웃 설정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,18 @@
 import React, { ReactElement } from "react";
+import Header from "@/components/common/Header";
+import styles from "./MainPage.scss";
 
 function MainPage(): ReactElement {
   return (
-    <div>
-      <div>Mainpage</div>
-    </div>
+    <>
+      <Header />
+      <main>
+        <div className={styles.container}>
+          <div className={styles.leftWrapper}> left </div>
+          <div className={styles.rightWrapper}> right </div>
+        </div>
+      </main>
+    </>
   );
 }
 
